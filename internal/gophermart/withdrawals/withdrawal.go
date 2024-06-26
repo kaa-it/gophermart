74d5670594a666bb8c101fe1909e55551c668cb3
Andrey Kruglov <kruglov.it@gmail.com>
2024-06-26 12:30:46 +0300

    fix: withdrawal marshal fix

--- internal/gophermart/withdrawals/withdrawal.go
+++ internal/gophermart/withdrawals/withdrawal.go
@@ -1,6 +1,7 @@
 package withdrawals
 
 import (
+	"encoding/json"
 	"time"
 
 	"github.com/shopspring/decimal"
@@ -10,4 +11,18 @@ type Withdrawal struct {
 	Order       string          `json:"order"`
 	Sum         decimal.Decimal `json:"sum"`
 	ProcessedAt time.Time       `json:"processed_at"`
+}
+
+func (w Withdrawal) MarshalJSON() ([]byte, error) {
+	type WithdrawalAlias Withdrawal
+
+	alias := struct {
+		WithdrawalAlias
+		Sum float64 `json:"sum"`
+	}{
+		WithdrawalAlias: WithdrawalAlias(w),
+		Sum:             w.Sum.InexactFloat64(),
+	}
+
+	return json.Marshal(alias)
 }
