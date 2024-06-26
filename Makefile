592f448d3d2ff8693c128c058c8ec6be28b72de2
Andrey Kruglov <kruglov.it@gmail.com>
2024-06-26 12:12:14 +0300

    fix: marshal order

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ build:
 	go build -o gophermart ./cmd/gophermart ;
 
 run_server:
-	./gophermart -d "postgres://ak:postgres@localhost:5432/gophermart"
+	./gophermart -d "postgres://ak:postgres@localhost:5432/gophermart" -a "http://localhost:5555"
--- internal/gophermart/orders/order.go
+++ internal/gophermart/orders/order.go
@@ -1,6 +1,7 @@
 package orders
 
 import (
+	"encoding/json"
 	"time"
 
 	"github.com/shopspring/decimal"
@@ -19,4 +20,24 @@ type Order struct {
 	Status     string           `json:"status"`
 	Accrual    *decimal.Decimal `json:"accrual,omitempty"`
 	UploadedAt time.Time        `json:"uploaded_at"`
+}
+
+func (o Order) MarshalJSON() ([]byte, error) {
+	type OrderAlias Order
+
+	var accrual *float64
+	if o.Accrual != nil {
+		accrualFloat := o.Accrual.InexactFloat64()
+		accrual = &accrualFloat
+	}
+
+	alias := struct {
+		OrderAlias
+		Accrual *float64 `json:"accrual,omitempty"`
+	}{
+		OrderAlias: OrderAlias(o),
+		Accrual:    accrual,
+	}
+
+	return json.Marshal(alias)
 }
